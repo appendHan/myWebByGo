c6c0c8a5def0be9b60108f2ca50b0e37b7985e80
张思航 <zhangsihang@192.168.0.150>
2018-03-19 22:15:18 +0800

    add driver/mysql

--- database/mysql.go
+++ database/mysql.go
@@ -1,1 +1,16 @@
 package mysql
+
+import (
+	"database/sql"
+ _ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+func main() {
+	db, err := sql.Open("mysql",
+		"root:root@tcp(127.0.0.1:3306)/go_main")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+}
